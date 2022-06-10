691b827d0ac7d9b4fa863a6196aa5722d7cd8dd4
Nazar Bychyk <nazar1bychyk@gmail.com>
2022-06-10 15:19:31 +0200

    clear some code in Clock. Added class main__bottom and changed style in main__bottom paragraf

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import { useCurrentDate } from './useCurrentDate.js'
+import { useState, useEffect } from "react";
+import "./style.css";
 
 const Clock = () => {
 
-    const date = useCurrentDate();
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
 
     return (
         <p className='date'>
-            {date.toLocaleString(date, {
-                weekday: "long",
+            {date.toLocaleString(undefined, {
                 hour: "numeric",
                 minute: "numeric",
                 second: "numeric",
--- src/Main/index.js
+++ src/Main/index.js
@@ -5,9 +5,9 @@ const Main = ({ content, title }) => {
 
     return (<main className="main">
         {title}
-        <h1 className="article__header">Kalkulator walutowy</h1>
+        <h1 className="main__header">Kalkulator walutowy</h1>
         {content}
-        <p>kurs z dnia 10.05.2022</p>
+        <p className="main__bottom">kurs z dnia 10.05.2022</p>
     </main>)
 }
 
